378d8b38cf8a71eddfda8258a7f79b4b79d3538c
mayank thole <119438990+mayankthole@users.noreply.github.com>
2025-03-19 11:58:01 +0530

    Add files via upload

---
+++ Strategy_Selection_System/run_platform.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from trading_strategy_selector import StrategySelectionApp
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = StrategySelectionApp(root)
+    root.mainloop()
